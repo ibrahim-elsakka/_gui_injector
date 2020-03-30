bafaf559ea8ebe732d1847ffd4e2e44a8e6a07d6
kage <kage>
2020-03-31 00:44:30 +0200

    back before crash

--- _gui_injector/main.cpp
+++ _gui_injector/main.cpp
@@ -4,38 +4,21 @@
 #include <Windows.h>
 #include <qmessagebox.h>
 
-#include "DarkStyle.h"
-#include "framelesswindow.h"
-#include "mainwindow.h"
 
 
+//fuck off
 int main(int argc, char* argv[])
 {
 
+
+    qApp->quit();
 
     // Restart Application loop
     int currentExitCode = 0;
     do {
         QApplication a(argc, argv);
-
-        // style our application with custom dark style
-        a.setStyle(new DarkStyle);
-
-        // create frameless window (and set windowState or title)
-        FramelessWindow framelessWindow;
-        //framelessWindow.setWindowState(Qt::WindowMaximized);
-        //framelessWindow.setWindowTitle("test title");
-        //framelessWindow.setWindowIcon(a.style()->standardIcon(QStyle::SP_DesktopIcon));
-
-        // create our mainwindow instance
-        MainWindow* mainWindow = new MainWindow;
-
-        // add the mainwindow to our custom frameless window
-        framelessWindow.setContent(mainWindow);
-        framelessWindow.show();
-
-        return a.exec();
-
+        GuiMain w;
+        w.show();
         currentExitCode = a.exec();
     } while (currentExitCode == GuiMain::EXIT_CODE_REBOOT);
 
